32a36dcfc8a1e4b34895453634d09cd68ae3e3bc
Henchenski <andrehenc@gmail.com>
2024-01-03 16:47:04 -0300

    Do exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
 const removeFromArray = function() {
+    let result = []
+    
+    for (let i = 1; i < arguments.length; i++) {
+        let index = arguments[0].indexOf(arguments[i])
+        if (index >= 0) {
+            arguments[0].splice(index, 1)
+        }
+    }
 
+    return arguments[0]
 };
 
 // Do not edit below this line
