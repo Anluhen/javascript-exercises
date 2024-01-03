f1f021838c960cc1519b1b499f6cb67d4be4274e
Henchenski <andrehenc@gmail.com>
2024-01-03 17:59:15 -0300

    Do exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
 const sumAll = function() {
+    let sum = 0
 
+    if (Math.min(...arguments) < 0) {
+        return 'ERROR'
+    }
+
+    for (let i = 0; i < arguments.length; i++) {
+        if (typeof arguments[i] != "number") {
+            return 'ERROR'
+        }
+    }
+
+    for (let i = Math.min(...arguments); i <= Math.max(...arguments); i++) {
+        sum += i;
+    }
+
+    return sum
 };
+
+console.log(`Testing... ${sumAll(1, 4)}`)
 
 // Do not edit below this line
 module.exports = sumAll;
