6fb5e09601958daadaaf3a9d229f89bcfb28b125
Henchenski <andrehenc@gmail.com>
2024-01-06 19:06:09 -0300

    Do exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(memberNumber) {
+    arrayFibonacci = [1, 1]
 
+    if (memberNumber == 0) {
+        return 0
+    } else if (memberNumber < 0) {
+        return "OOPS"
+    } else {
+        for (let i = arrayFibonacci.length; i <= memberNumber; i++) {
+            arrayFibonacci.push(arrayFibonacci[arrayFibonacci.length - 2] + arrayFibonacci[arrayFibonacci.length - 1])
+        }
+        return arrayFibonacci[memberNumber - 1]
+    }
 };
 
 // Do not edit below this line
