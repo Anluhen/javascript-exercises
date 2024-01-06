f3336d6e06701abf4360897c496b9e319824dc5a
Henchenski <andrehenc@gmail.com>
2024-01-06 13:25:32 -0300

    Do exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
 const add = function() {
-	
+	return arguments[0] + arguments[1]
 };
 
 const subtract = function() {
-	
+	return arguments[0] - arguments[1]
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce(
+    (sum, currentValue) => sum + currentValue,
+    0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce(
+    (product, currentValue) => product * currentValue)
 };
 
 const power = function() {
-	
+	return arguments[0] ** arguments[1]
 };
 
 const factorial = function() {
-	
+	let i = 1
+  let result = 1
+  do {
+    result *= i
+    i++
+  } while ( i <= arguments[0])
+
+  return result
 };
 
 // Do not edit below this line
