2f23c3266d5a1bf33e3d8094351cbb648db69bbf
Henchenski <andrehenc@gmail.com>
2024-01-06 18:34:08 -0300

    Do exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (s) {
+    s = s.replace(/\W/g, "")
 
+    if (Array.from(s.toLowerCase()).toString() == Array.from(s.toLowerCase()).reverse().toString()) {
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
