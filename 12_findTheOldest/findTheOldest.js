288021dfb065ee0652125fe004d4ced611756b8a
Henchenski <andrehenc@gmail.com>
2024-01-06 22:13:52 -0300

    Do exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const TODAY = new Date().getFullYear()
+    let oldest = {}
+    let oldestAge = 0
+    
 
+    array.forEach(element => {
+        if (element.yearOfDeath === undefined){
+            age = TODAY - element.yearOfBirth
+        } else {
+            age = element.yearOfDeath - element.yearOfBirth
+        }
+        
+        if (age > oldestAge){
+            oldest = element
+            oldestAge = age
+        }
+    });
+
+    return oldest
 };
 
 // Do not edit below this line
